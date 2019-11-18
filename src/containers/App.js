3ee2bdea571f23c48e9f5cfa0ec71c0548fbfce2
Derek <derekj77@gmail.com>
2019-11-18 16:15:56 -0600

    Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,7 +12,10 @@ import { setSearchField } from "../actions";
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        errror: state.requestRobots.error
     };
 };
 
